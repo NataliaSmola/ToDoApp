39de0114be0d1362ae248650b1c2e672202a93bf
Natalia <smolutka15@gmail.com>
2021-04-06 19:16:42 +0200

    Update websocke on client's side

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import io from 'socket.io-client';
 
 class App extends React.Component {
+  state = {
+    tasks: [
+      {name:'dupka', id:1},
+      {name:'srobka', id:2},
+      {name:'fff', id:3}
+  ],
+  taskName: '',
+};
+
+  componentDidMount() {
+    this.socket = io.connect("http://localhost:8000");
+    this.socket.on('removeTask', (id) => this.removeTask(id));
+    this.socket.on('addTask', (task) => this.addTask(task));
+  };
+
+  handleChange(event) {
+   this.setState({taskName: event.target.value});
+ }
+
+  removeTask(id){
+    this.setState({
+      tasks: this.state.tasks.filter(task => task.id !== id)
+    })
+    this.socket.emit('removeTask', id);
+  }
+
+  submitForm(event){
+     event.preventDefault();
+     const newTask = {
+       name: this.state.taskName};
+     this.addTask(newTask);
+     this.socket.emit('addTask', newTask);
+     this.setState({
+       task: [...this.state.tasks, newTask]
+     })
+  }
+
+  addTask(task){
+    this.state.tasks.push(task);
+    this.setState({
+      tasks: this.state.tasks,
+    })
+  }
+
+
   render() {
+    const {tasks, taskName} = this.state;
     return (
       <div className="App">
         <header>
@@ -10,11 +57,21 @@ class App extends React.Component {
         <section className="tasks-section" id="tasks-section">
           <h2>Tasks</h2>
           <ul className="tasks-section__list" id="tasks-list">
-            <li class="task">Shopping <button class="btn btn--red">Remove</button></li>
-            <li class="task">Go out with a dog <button class="btn btn--red">Remove</button></li>
+          {tasks.map((task) =>(
+            <li key={task.id} className="task">{task.name}<button className="btn btn--red"
+            onClick={() => this.removeTask(task.id)}>Remove</button></li>
+          ))}
           </ul>
-          <form id="add-task-form">
-            <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" />
+          <form id="add-task-form" onSubmit={(submit) => this.submitForm(submit)}>
+            <input
+            className="text-input"
+            autoComplete="off"
+            type="text"
+            placeholder="Type your description"
+            id="task-name"
+            required='required'
+            value={taskName}
+            onChange={(event) => this.setState({taskName: event.target.value})}  />
             <button className="btn" type="submit">Add</button>
           </form>
         </section>
