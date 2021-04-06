2e6b1d200518d526d7929e0076257d8e22899c1d
Natalia <smolutka15@gmail.com>
2021-04-06 13:51:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
+const tasks = [];
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
 });
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  io.to(socket.id).emit('updateData', tasks);
+
+  socket.on('addTask', (newTask) => {
+      console.log('New task:' + newTask + 'from' + socket.id);
+      tasks.push(newTask);
+      socket.broadcast.emit('addTask', newTask);
+    });
+
+  socket.on('removeTask', (taskToRemove) => {
+    console.log('Please remove' + taskToRemove);
+    tasks.splice(taskToRemove, 1);
+    socket.broadcast.emit('removeTask', taskToRemove);
+  });
 });
